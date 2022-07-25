44635d300fcfab501a5c549d2a5e897cef352fa7
IgorRyabenko <riabenko.igor@gmail.com>
2022-07-25 12:59:10 +0300

    refactoring

--- src/shared/api/films.js
+++ src/shared/api/films.js
@@ -1,32 +1,34 @@
 import axios from 'axios';
 
-const apiKEY = '196da2d2dd4f95fd9bfe0927ab5d4d33';
-
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: '196da2d2dd4f95fd9bfe0927ab5d4d33',
+    language: 'en-US'
+   }
 });
 
 export const getFilms = async () => {
-  const { data } = await instance.get(`trending/all/day?api_key=${apiKEY}`);
+  const { data } = await instance.get('trending/all/day');
   return data;
 };
 
 export const fetchFilmsWithId = async id => {
-  const response = await instance.get(`/movie/${id}?api_key=${apiKEY}&language=en-US`);
+  const response = await instance.get(`/movie/${id}`);
   return response;
 };
 
 export const fetchFilmsWithQuery = async searchQuery => {
-  const response = await instance.get(`/search/movie?api_key=${apiKEY}&language=en-US&page=1&include_adult=false&query=${searchQuery}`)
+  const response = await instance.get(`/search/movie?&page=1&include_adult=false&query=${searchQuery}`)
   return response;
 }
 
 export const fetchCastWithId = async id => {
-  const response = await instance.get(`/movie/${id}/credits?api_key=${apiKEY}&language=en-US`);
+  const response = await instance.get(`/movie/${id}/credits`);
   return response;
 }
 
 export const fetchReviewsWithId = async id => {
-  const response = await instance.get(`/movie/${id}/reviews?api_key=${apiKEY}&language=en-US`);
+  const response = await instance.get(`/movie/${id}/reviews`);
   return response;
 }
