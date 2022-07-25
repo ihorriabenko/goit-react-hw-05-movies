aa3c779540eafe8f0240591d184ba29ed7debc18
iryabenkoff <100082550+iryabenkoff@users.noreply.github.com>
2022-07-25 06:48:27 +0000

    Deploying to gh-pages from @ iryabenkoff/goit-react-hw-05-movies@d2537444ad379dfd32578f7341bfe87f1c743e15 🚀

---
+++ static/js/93.026f66b0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[93],{3093:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(501),a=r(184),c=function(){return(0,a.jsx)("main",{children:(0,a.jsxs)("div",{className:"container",children:[(0,a.jsx)("h2",{children:"Page not found"}),(0,a.jsx)(t.rU,{to:"/",children:"To home page"})]})})}}}]);
+//# sourceMappingURL=93.026f66b0.chunk.js.map
